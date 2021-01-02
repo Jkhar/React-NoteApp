1cada0744e5d31feaba7da31800a620c4bfbb9ad
Jkhar <ayoubjkhar@gmail.com>
2021-01-02 16:50:12 +0100

    add ptj

--- backend/routes/note_routes.js
+++ backend/routes/note_routes.js
@@ -1,6 +1,9 @@
 const express = require('express') ; 
 const  {getAllNotesNames , getAllNotes ,addNote ,updateNote , deleteNote , getNoteById} = require('../controllers/note_controllers') ;
 
+const  { AddUserEntreprise ,AddUserIndividuel , UserConnection } = require('../controllers/user_controllers/user_controllers') ;
+const  { DeposerAnnonce , getAllAnnonces  } = require('../controllers/annonce_controllers/annonce_controllers') ;
+ 
 const gestion_notes_routes = express.Router() ; 
 
 // Creation du Routes 
@@ -10,5 +13,16 @@ gestion_notes_routes.get('/note/:id' ,getNoteById) ;
 gestion_notes_routes.post('/note/add',addNote) ; 
 gestion_notes_routes.put('/note/update/:id',updateNote) ;
 gestion_notes_routes.delete('/note/delete/:id',deleteNote) ;
+
+gestion_notes_routes.post('/userEntreprise/add',AddUserEntreprise) ; 
+gestion_notes_routes.post('/userIndividuel/add',AddUserIndividuel) ;
+
+gestion_notes_routes.post('/userConnection',UserConnection) ;  
+
+// pour les Annonces 
+
+gestion_notes_routes.post('/DeposerAnnonce',DeposerAnnonce) ;   
+gestion_notes_routes.get('/ListAnnonces' ,getAllAnnonces) ;
+
 
 module.exports = {gestion_notes_routes} ; 
